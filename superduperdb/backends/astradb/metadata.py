48e7f1f252ec9bc9fd94cf65a789e16f82646bc1
hakeemsyd <hakeemsyd@gmail.com>
2024-02-28 17:03:28 +0500

    fix: projection and filter in show jobs method

--- superduperdb/backends/astradb/metadata.py
+++ superduperdb/backends/astradb/metadata.py
@@ -16,11 +16,11 @@ class AstraMetaDataStore(MetaDataStore):
     :param name: Name of database to host filesystem
     """
 
-    table_names=["_meta","_cdc_tables","_objects","_jobs","_parent_child_mappings"]
+    table_names = ["_meta", "_cdc_tables", "_objects", "_jobs", "_parent_child_mappings"]
 
     def __init__(
-        self,
-        conn: AstraDB, name: str
+            self,
+            conn: AstraDB, name: str
     ) -> None:
         self.name = name
         self.db = conn
@@ -47,7 +47,6 @@ class AstraMetaDataStore(MetaDataStore):
         self.job_collection = AstraDBCollection(collection_name=table_names[3], astra_db=conn)
         self.parent_child_mappings = AstraDBCollection(collection_name=table_names[4], astra_db=conn)
 
-
     def url(self):
         """
         Databackend connection url
@@ -57,10 +56,10 @@ class AstraMetaDataStore(MetaDataStore):
     def drop(self, force: bool = False):
         if not force:
             if not click.confirm(
-                f'{Colors.RED}[!!!WARNING USE WITH CAUTION AS YOU '
-                f'WILL LOSE ALL DATA!!!]{Colors.RESET} '
-                'Are you sure you want to drop all meta-data? ',
-                default=False,
+                    f'{Colors.RED}[!!!WARNING USE WITH CAUTION AS YOU '
+                    f'WILL LOSE ALL DATA!!!]{Colors.RESET} '
+                    'Are you sure you want to drop all meta-data? ',
+                    default=False,
             ):
                 logging.warn('Aborting...')
         self.db.delete_collection(self.meta_collection.name)
@@ -97,50 +96,50 @@ class AstraMetaDataStore(MetaDataStore):
         return self.meta_collection.update_one(filter={'key': key}, update={'$set': {'value': value}})
 
     def get_latest_version(
-        self, type_id: str, identifier: str, allow_hidden: bool = False
+            self, type_id: str, identifier: str, allow_hidden: bool = False
     ) -> int:
         try:
             if allow_hidden:
-                    result = self.component_collection.find(filter = {'identifier': identifier, 'type_id': type_id})
-                    distinct_values = []
-                    for doc in result:
-                        if doc['version'] not in distinct_values:
-                            distinct_values.append(doc['version'])
-                    return sorted(distinct_values)[-1]
+                result = self.component_collection.find(filter={'identifier': identifier, 'type_id': type_id})
+                distinct_values = []
+                for doc in result:
+                    if doc['version'] not in distinct_values:
+                        distinct_values.append(doc['version'])
+                return sorted(distinct_values)[-1]
             else:
-                    result=self.component_collection.find(
-                        filter = {
-                            '$or': [
-                                {
-                                    'identifier': identifier,
-                                    'type_id': type_id,
-                                    'hidden': False,
-                                },
-                                {
-                                    'identifier': identifier,
-                                    'type_id': type_id,
-                                    'hidden': {'$exists': 0},
-                                },
-                            ]
-                        },
-                    )
-                    distinct_values = []
-                    for doc in result:
-                        if doc['version'] not in distinct_values:
-                            distinct_values.append(doc['version'])
-                    return sorted(distinct_values)[-1]
+                result = self.component_collection.find(
+                    filter={
+                        '$or': [
+                            {
+                                'identifier': identifier,
+                                'type_id': type_id,
+                                'hidden': False,
+                            },
+                            {
+                                'identifier': identifier,
+                                'type_id': type_id,
+                                'hidden': {'$exists': 0},
+                            },
+                        ]
+                    },
+                )
+                distinct_values = []
+                for doc in result:
+                    if doc['version'] not in distinct_values:
+                        distinct_values.append(doc['version'])
+                return sorted(distinct_values)[-1]
         except IndexError:
             raise FileNotFoundError(f'Can\'t find {type_id}: {identifier} in metadata')
 
     def update_job(self, identifier: str, key: str, value: t.Any):
         return self.job_collection.update_one(
-            filter={'identifier': identifier},update={'$set': {key: value}}
+            filter={'identifier': identifier}, update={'$set': {key: value}}
         )
 
     def show_components(self, type_id: str, **kwargs) -> t.List[t.Union[t.Any, str]]:
         # TODO: Should this be sorted?
         result = self.component_collection.find(
-            filter = {'type_id': type_id, **kwargs}
+            filter={'type_id': type_id, **kwargs}
         )
         distinct_values = []
         for doc in result:
@@ -148,13 +147,11 @@ class AstraMetaDataStore(MetaDataStore):
                 distinct_values.append(doc['identifier'])
         return distinct_values
 
-    # TODO: Why is this is needed to prevent failures in CI?
-    @tenacity.retry(stop=tenacity.stop_after_attempt(10))
     def show_component_versions(
-        self, type_id: str, identifier: str
+            self, type_id: str, identifier: str
     ) -> t.List[t.Union[t.Any, int]]:
         result = self.component_collection.distinct(
-            filter = {'type_id': type_id, 'identifier': identifier}
+            filter={'type_id': type_id, 'identifier': identifier}
         )
         distinct_values = []
         for doc in result:
@@ -169,12 +166,12 @@ class AstraMetaDataStore(MetaDataStore):
         status = {} if status is None else {'status': status}
         return list(
             self.job_collection.find(
-                filter = {'status': status ,'identifier': 1, '_id': 0, 'method': 1, 'status': 1, 'time': 1}
+                status, {'identifier': 1, '_id': 0, 'method': 1, 'status': 1, 'time': 1}
             )
         )
 
     def _component_used(
-        self, type_id: str, identifier: str, version: t.Optional[int] = None
+            self, type_id: str, identifier: str, version: t.Optional[int] = None
     ) -> bool:
         if version is None:
             members: t.Union[t.Dict, str] = {'$regex': f'^{identifier}/{type_id}'}
@@ -184,13 +181,13 @@ class AstraMetaDataStore(MetaDataStore):
         return bool(self.component_collection.count_documents(filter={'members': members}))
 
     def component_version_has_parents(
-        self, type_id: str, identifier: str, version: int
+            self, type_id: str, identifier: str, version: int
     ) -> int:
         doc = {'child': Component.make_unique_id(type_id, identifier, version)}
         return self.parent_child_mappings.count_documents(filter=doc)
 
     def delete_component_version(
-        self, type_id: str, identifier: str, version: int
+            self, type_id: str, identifier: str, version: int
     ):
         if self._component_used(type_id, identifier, version=version):
             raise Exception('Component version already in use in other components!')
@@ -208,14 +205,14 @@ class AstraMetaDataStore(MetaDataStore):
         )
 
     def _get_component(
-        self,
-        type_id: str,
-        identifier: str,
-        version: int,
-        allow_hidden: bool = False,
+            self,
+            type_id: str,
+            identifier: str,
+            version: int,
+            allow_hidden: bool = False,
     ) -> t.Dict[str, t.Any]:
         if not allow_hidden:
-            r =  self.component_collection.find_one(
+            r = self.component_collection.find_one(
                 filter={
                     '$or': [
                         {
@@ -249,11 +246,11 @@ class AstraMetaDataStore(MetaDataStore):
         ]
 
     def _replace_object(
-        self,
-        info: t.Dict[str, t.Any],
-        identifier: str,
-        type_id: str,
-        version: int,
+            self,
+            info: t.Dict[str, t.Any],
+            identifier: str,
+            type_id: str,
+            version: int,
     ) -> None:
         self.component_collection.find_one_and_replace(
             filter={'identifier': identifier, 'type_id': type_id, 'version': version},
@@ -261,12 +258,12 @@ class AstraMetaDataStore(MetaDataStore):
         )
 
     def _update_object(
-        self,
-        identifier: str,
-        type_id: str,
-        key: str,
-        value: t.Any,
-        version: int,
+            self,
+            identifier: str,
+            type_id: str,
+            key: str,
+            value: t.Any,
+            version: int,
     ):
         return self.component_collection.update_one(
             filter={'identifier': identifier, 'type_id': type_id, 'version': version},
@@ -277,11 +274,11 @@ class AstraMetaDataStore(MetaDataStore):
         if stream not in ('stdout', 'stderr'):
             raise ValueError(f'stream is "{stream}", should be stdout or stderr')
         self.job_collection.update_one(
-            filter={'identifier': identifier},update= {'$push': {stream: msg}}
+            filter={'identifier': identifier}, update={'$push': {stream: msg}}
         )
 
     def hide_component_version(
-        self, type_id: str, identifier: str, version: int
+            self, type_id: str, identifier: str, version: int
     ) -> None:
         self.component_collection.update_one(
             filter={'type_id': type_id, 'identifier': identifier, 'version': version},
